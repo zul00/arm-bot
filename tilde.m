c81d1cc48706106e42bef42fce3f718710eeb6d6
zul00 <zul.zulkarnaen@gmail.com>
2017-05-22 23:53:02 +0000

    Updated tilde operator

--- tilde.m
+++ tilde.m
@@ -1,10 +1,21 @@
-function [stat, tT] = tilde(T)
+function [stat, tT] = Ttilde(T)
 
-    tT = [
-        0       -T(3)   -T(2);
-        T(3)    0       -T(1);
-        -T(2)   T(1)    0
-        ];
+% Obtain omega and v
+omega = T(1:3);
+v = T(4:6);
 
-    stat = 0;
+% Omega tilde
+omega_t = [
+0       -T(3)   -T(2);
+T(3)    0       -T(1);
+-T(2)   T(1)    0
+];
+
+% Generate T tilde
+tT = [
+omega_t     v;
+zeros(1,3)  0
+];
+
+stat = 0;
 end
