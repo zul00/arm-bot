a942e24ace92d96329b83109d84a87e6e9e0924c
zul00 <zul.zulkarnaen@gmail.com>
2017-05-22 23:36:04 +0000

    Defined tilde operator without checking the input

--- tilde.m
+++ tilde.m
@@ -1,3 +1,10 @@
-function tilde(T)
+function [stat, tT] = tilde(T)
 
+    tT = [
+        0       -T(3)   -T(2);
+        T(3)    0       -T(1);
+        -T(2)   T(1)    0
+        ];
+
+    stat = 0;
 end
