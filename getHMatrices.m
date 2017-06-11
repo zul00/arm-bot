3a5dd08c215d93d0c06559262e03410145a774a9
zul00 <zul.zulkarnaen@gmail.com>
2017-06-11 12:59:24 +0000

    Fixed Brocket equation for three arm robot

--- getHMatrices.m
+++ getHMatrices.m
@@ -2,57 +2,27 @@ clear all;
 
 pi = pi();
 
-q = [0; 0; 0];
+q = [pi/2; pi/2; 0];
 
-L1 = 1;
+L1 = 3;
 L2 = 2;
-L3 = 3;
+L3 = 1;
 
-%% Initial transformation matrix
-% H1
-theta = pi/2;
-R = [
-    cos(theta)   0   sin(theta);
-    0            1   0;
-    -sin(theta)  0   cos(theta)
-];
-T = [L1;0;0];
-
-H1 = [
-    R           T;
-    zeros(1,3)  1
-];
-
-% H2
-theta = pi()/2;
-R = [
-    cos(theta)   0   sin(theta);
-    0            1   0;
-    -sin(theta)  0   cos(theta)
-];
-T = [L2;0;0];
-H2 = [
-    R           T;
-    zeros(1,3)  1
-];
-
-% H3
-T = [L2+L3;0;L1];
-H3 = [
-    R          T;
+% Hee(0)
+T = [0;0;L1+L2+L3];
+Hee = [
+    eye(3)      T;
     zeros(1,3)  1
 ];
 
 % Unit twist
-T1 = [0; 0;  1; 0; 0; 0];
-T2 = [0; 0; 0; 1; 0; 0];
-T3 = [0; -1; 0; 0; 0; 0];
+T1 = [0; 0; 1;    0; 0; 0];
+T2 = [1; 0; 0;    0; L1; 0];
+T3 = [1; 0; 0;    0; L1+L2; 0];
 
 [stat tT1] = tildeTwist(T1);
 [stat tT2] = tildeTwist(T2);
 [stat tT3] = tildeTwist(T3);
 
 % Brocket Eq
-H1q = expm(tT1*q(1))*H1;
-H2q = expm(tT1*q(1))*expm(tT2*q(2))*H2;
-H3q = expm(tT1*q(1))*expm(tT2*q(2))*expm(tT3*q(3))*H3;
+Heeq = expm(tT1*q(1))*expm(tT2*q(2))*expm(tT3*q(3))*Hee;
