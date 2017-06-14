cc19a28d71747f819663ad551ab02248fbe7ab97
zul00 <zul.zulkarnaen@gmail.com>
2017-06-14 13:30:55 +0000

    Updated unit twist calculation in jacobian

--- getJacobian.m
+++ getJacobian.m
@@ -1,19 +1,44 @@
 clear all
 
 %q    X     ;Y      ;Z
-q  = [pi/2  ;pi/2   ;0];
+q  = [pi/3  ;pi/3   ;0];
 qd = [pi    ;1      ;0];
 
-%% Jacobian
+L = [3  2  1]';
+
+%% Unit twist
+% Joint 1
+T1_00 = [0  0  1  0  0  0]';
+
+% Joint 2
+T2_11 = [1  0  0  0  0  0]';
+%> Change coordinates to 0
+P10    = [0  0  L(1)];
+adjH10 = adj('z', q(1), P10);
+T2_10  = adjH10 * T2_11;
+
+% Joint 3
+T3_22 = [1  0  0  0  0  0]';
+%> Change coordinates to 0
+P21    = [0  0  L(2)];
+adjH21 = adj('x', q(2), P21);
+adjH20 = adjH10 * adjH21;
+T3_20 = adjH10 * adjH21 * T3_22;
+
+
+
+
+
 w1q = [0; 0; 1];
 T1q = [w1q; 0; 0 ;0];
 
 w2q  = [cos(qd(1));     sin(qd(1));    0];
-rw2q = [-L1*sin(qd(1)); L1*cos(qd(1)); 0];
+rw2q = [-L(1)*sin(qd(1)); L(1)*cos(qd(1)); 0];
 T2q = [w2q; rw2q];
 
 T3q = [0; 0; 1; 0; 0 ;0];
 
+%% Generate Jacobian
 J = [T1q T2q T3q];
 
 Tq = J * qd;
