4acf94183429acd1dffee411c4575dae7815df30
zul00 <zul.zulkarnaen@gmail.com>
2017-06-14 11:52:29 +0000

    Added transitional homogenous transform matrix

--- getHMatrices.m
+++ getHMatrices.m
@@ -8,21 +8,46 @@ L1 = 3;
 L2 = 2;
 L3 = 1;
 
-% Hee(0)
+% Homogenous Transformation Matrix
+% Joint1
+P = [0;0;0];
+H10_0 = [
+    eye(3)      P;
+    zeros(1,3)  1
+];
+
+% Joint2
+P = [0;0;L1];
+H20_0 = [
+    eye(3)      P;
+    zeros(1,3)  1
+];
+
+% Joint3
+P = [0;0;L1+L2];
+H30_0 = [
+    eye(3)      P;
+    zeros(1,3)  1
+];
+
+% End Effector
 P = [0;0;L1+L2+L3];
-Hee0 = [
+Hee0_0 = [
     eye(3)      P;
     zeros(1,3)  1
 ];
 
 % Unit twist
-T100 = [0; 0; 1;    0; 0; 0];
-T210 = [1; 0; 0;    0; L1; 0];
-T320 = [1; 0; 0;    0; L1+L2; 0];
+T10_0 = [0; 0; 1;    0; 0; 0];
+T21_0 = [1; 0; 0;    0; L1; 0];
+T32_0 = [1; 0; 0;    0; L1+L2; 0];
 
-[stat tT100] = tildeTwist(T100);
-[stat tT210] = tildeTwist(T210);
-[stat tT320] = tildeTwist(T320);
+[stat tT10_0] = tildeTwist(T10_0);
+[stat tT21_0] = tildeTwist(T21_0);
+[stat tT32_0] = tildeTwist(T32_0);
 
 % Brocket Eq
-Heeq = expm(tT100*q(1)) * expm(tT210*q(2)) * expm(tT320*q(3)) * Hee0;
+H10_q  = expm(tT10_0*q(1)) * H10_0;
+H20_q  = expm(tT10_0*q(1)) * expm(tT21_0*q(2)) * H20_0;
+H30_q  = expm(tT10_0*q(1)) * expm(tT21_0*q(2)) * expm(tT32_0*q(3)) * H30_0;
+Hee0_q = expm(tT10_0*q(1)) * expm(tT21_0*q(2)) * expm(tT32_0*q(3)) * Hee0_0;
