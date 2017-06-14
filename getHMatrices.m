313c308e5de89070f4d2091a33e0009ef2240bd0
zul00 <zul.zulkarnaen@gmail.com>
2017-06-14 11:08:47 +0000

    Changed variable naming

--- getHMatrices.m
+++ getHMatrices.m
@@ -9,20 +9,20 @@ L2 = 2;
 L3 = 1;
 
 % Hee(0)
-T = [0;0;L1+L2+L3];
-Hee = [
-    eye(3)      T;
+P = [0;0;L1+L2+L3];
+Hee0 = [
+    eye(3)      P;
     zeros(1,3)  1
 ];
 
 % Unit twist
-T1 = [0; 0; 1;    0; 0; 0];
-T2 = [1; 0; 0;    0; L1; 0];
-T3 = [1; 0; 0;    0; L1+L2; 0];
+T100 = [0; 0; 1;    0; 0; 0];
+T210 = [1; 0; 0;    0; L1; 0];
+T320 = [1; 0; 0;    0; L1+L2; 0];
 
-[stat tT1] = tildeTwist(T1);
-[stat tT2] = tildeTwist(T2);
-[stat tT3] = tildeTwist(T3);
+[stat tT100] = tildeTwist(T100);
+[stat tT210] = tildeTwist(T210);
+[stat tT320] = tildeTwist(T320);
 
 % Brocket Eq
-Heeq = expm(tT1*q(1))*expm(tT2*q(2))*expm(tT3*q(3))*Hee;
+Heeq = expm(tT1100*q(1)) * expm(tT210*q(2))*expm(tT320*q(3))*Hee;
