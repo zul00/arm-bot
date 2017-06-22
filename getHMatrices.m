f570864285b8b73bb5134f0f06e00053515f12cb
zul00 <zul.zulkarnaen@gmail.com>
2017-06-22 09:34:08 +0000

    Updated Brocket

--- getHMatrices.m
+++ getHMatrices.m
@@ -1,55 +1,58 @@
-function [H10 H20 H30 Hee0] = getHMatrices(q, L)
+function [H1 H2 H3 He] = getHMatrices(q, L)
 
-% Homogenous Transformation Matrix
-% Joint 1
-P = [0 0 0]';
-H10_0 = [
-    eye(3)      P;
+%% Homogenous Transformation Matrix
+% Body 1
+P1   = [0 0 0]';
+H1_0 = [
+    eye(3)      P1;
     zeros(1,3)  1
 ];
 
-% Joint 2
-P = [0 0 L(1)]';
-H20_0 = [
-    eye(3)      P;
+% Body 2
+P2   = [0 0 L(1)]';
+H2_0 = [
+    eye(3)      P2;
     zeros(1,3)  1
 ];
 
-% Joint 3
-P = [0 0 L(1)+L(2)]';
-H30_0 = [
-    eye(3)      P;
+% Body 3
+P3   = [0 0 L(1)+L(2)]';
+H3_0 = [
+    eye(3)      P3;
     zeros(1,3)  1
 ];
 
 % End Effector
-P = [0 0 L(1)+L(2)+L(3)]';
-Hee0_0 = [
-    eye(3)      P;
+Pe   = [0 0 L(1)+L(2)+L(3)]';
+He_0 = [
+    eye(3)      Pe;
     zeros(1,3)  1
 ];
 
-% Unit twist
-w10_0 = [0  0  1]';
-r10   = [0  0  0]';
-T10_0 = [w10_0;  cross(r10, w10_0)];
+%% Unit twist
+% Body 1
+w1 = [0  0  1]';
+r1 = [0  0  0]';
+T1 = [w1;  cross(r1, w1)];
 
-w21_0 = [1  0  0]';
-r20   = [0  0  L(1)]';
-T21_0 = [w21_0;  cross(r20, w21_0)];
+% Body 2
+w2 = [1  0  0]';
+r2 = [0  0  L(1)]';
+T2 = [w2;  cross(r2, w2)];
 
-w32_0 = [1  0  0]';
-r30   = [0  0  L(1)+L(2)]';
-T32_0 = [w32_0;  cross(r30, w32_0)];
+% Body 3
+w3 = [1  0  0]';
+r3 = [0  0  L(1)+L(2)]';
+T3 = [w3;  cross(r3, w3)];
 
-% Tilde operator
-tT10_0 = tildeTwist(T10_0);
-tT21_0 = tildeTwist(T21_0);
-tT32_0 = tildeTwist(T32_0);
+%% Tilde operator
+tT1 = tildeTwist(T1);
+tT2 = tildeTwist(T2);
+tT3 = tildeTwist(T3);
 
-% Brocket Eq
-H10  = expm(tT10_0*q(1)) * H10_0;
-H20  = expm(tT10_0*q(1)) * expm(tT21_0*q(2)) * H20_0;
-H30  = expm(tT10_0*q(1)) * expm(tT21_0*q(2)) * expm(tT32_0*q(3)) * H30_0;
-Hee0 = expm(tT10_0*q(1)) * expm(tT21_0*q(2)) * expm(tT32_0*q(3)) * Hee0_0;
+%% Brocket Equation
+H1 = expm(tT1*q(1)) * H1_0;
+H2 = expm(tT1*q(1)) * expm(tT2*q(2)) * H2_0;
+H3 = expm(tT1*q(1)) * expm(tT2*q(2)) * expm(tT3*q(3)) * H3_0;
+He = expm(tT1*q(1)) * expm(tT2*q(2)) * expm(tT3*q(3)) * He_0;
 end
