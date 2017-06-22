62eb620c46b48f2b77b1f43aff4f6986d3a2d0fa
zul00 <zul.zulkarnaen@gmail.com>
2017-06-22 10:03:13 +0000

    Transform jacobian to a function

--- getJacobian.m
+++ getJacobian.m
@@ -1,9 +1,4 @@
-clear all
-
-q  = [pi/2    ;0   ;0];
-qd = [0    ;0   ;1];
-
-L = [6  5  4]';
+function J = getJacobian(q, L)
 
 %% Unit twist
 % Body 1
@@ -13,7 +8,7 @@ T1 = [w1; cross(r1, w1)];
 
 % Body 2
 w2 = [cos(q(1))  -sin(q(1))  0]';
-r2 = [0  0  L(1)]';1
+r2 = [0  0  L(1)]';
 T2 = [w2; cross(r2, w2)];
 
 % Body 3
@@ -21,8 +16,7 @@ w3 = w2;
 r3 = [L(2)*sin(q(1))*sin(q(2))  -L(2)*cos(q(1))*sin(q(2))  L(1)+L(2)*cos(q(2))]';
 T3 = [w3; cross(r3, w3)];
 
-%% Jacobian
+%% Jacobian Equation
 J = [T1 T2 T3];
 
-%% Test Jacobian
-Tq = J * qd;
+end
--- main.m
+++ main.m
@@ -1,8 +1,15 @@
 clear all;
 
-pi = pi();
 
-q = [pi pi pi/2];
-L = [3 2 1];
+q  = [pi/2 pi/2 0]';
+qd = [1  0  0]';
+L  = [3  2  1];
 
+% Brocket
 [H10 H20 H30 Hee0] = getHMatrices(q, L);
+
+% Jacobian
+J = getJacobian(q, L);
+
+% Test Jacobian
+Tq = J * qd;
