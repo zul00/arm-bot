281f9fa8eea026aaabd9325449510b6c34e00753
zul00 <zul.zulkarnaen@gmail.com>
2017-07-06 09:16:42 +0000

    Plot error magnitude againts setpoint

--- robot_control_skeleton.m
+++ robot_control_skeleton.m
@@ -148,6 +148,16 @@ end
 %[tlog, dXlog] = getpoints(figX);
 
 %% Plot log
+err_Xm = log_Xsp - log_Xm;
+err_Ym = log_Ysp - log_Ym;
+err_Zm = log_Zsp - log_Zm;
+
+err_Xr = log_Xsp - log_Xr;
+err_Yr = log_Ysp - log_Yr;
+err_Zr = log_Zsp - log_Zr;
+
+err_Tm = (err_Xm.^2 + err_Ym.^2 + err_Zm.^2).^0.5;
+err_Tr = (err_Xr.^2 + err_Yr.^2 + err_Zr.^2).^0.5;
 
 figure(2)
 subplot(2,1,1)
@@ -163,8 +173,8 @@ subplot(2,1,1)
 subplot(2,1,2)
     title('X Error');
     hold on
-    plot(log_t, log_Xsp-log_Xm, 'Linewidth', 1.5)
-    plot(log_t, log_Xsp-log_Xr, 'Linewidth', 1.5)
+    plot(log_t, err_Xm, 'Linewidth', 1.5)
+    plot(log_t, err_Xr, 'Linewidth', 1.5)
     hold off
     xlabel('t');
     ylabel('cm');
@@ -184,8 +194,8 @@ subplot(2,1,1)
 subplot(2,1,2)
     title('Y Error');
     hold on
-    plot(log_t, log_Ysp-log_Ym, 'Linewidth', 1.5)
-    plot(log_t, log_Ysp-log_Yr, 'Linewidth', 1.5)
+    plot(log_t, err_Ym, 'Linewidth', 1.5)
+    plot(log_t, err_Yr, 'Linewidth', 1.5)
     hold off
     xlabel('t');
     ylabel('cm');
@@ -205,12 +215,36 @@ subplot(2,1,1)
 subplot(2,1,2)
     title('Z Error');
     hold on
-    plot(log_t, log_Zsp-log_Zm, 'Linewidth', 1.5)
-    plot(log_t, log_Zsp-log_Zr, 'Linewidth', 1.5)
+    plot(log_t, err_Zm, 'Linewidth', 1.5)
+    plot(log_t, err_Zr, 'Linewidth', 1.5)
     hold off
     xlabel('t');
     ylabel('cm');
     legend('model error', 'robot error')
+    
+figure(5)
+subplot(2,1,1)
+    title('Model');
+    hold on
+    plot(log_t, log_Xsp, 'Linewidth', 1.5)
+    plot(log_t, log_Ysp, 'Linewidth', 1.5)
+    plot(log_t, log_Zsp, 'Linewidth', 1.5)
+    plot(log_t, err_Tm, 'Linewidth', 2.0)
+    hold off
+    xlabel('t');
+    ylabel('cm');
+    legend('Xsp', 'Ysp', 'Zsp', 'Error')
+ subplot(2,1,2)
+    title('Robot');
+    hold on
+    plot(log_t, log_Xsp, 'Linewidth', 1.5)
+    plot(log_t, log_Ysp, 'Linewidth', 1.5)
+    plot(log_t, log_Zsp, 'Linewidth', 1.5)
+    plot(log_t, err_Tr, 'Linewidth', 2.0)
+    hold off
+    xlabel('t');
+    ylabel('cm');
+    legend('Xsp', 'Ysp', 'Zsp', 'Error')
 
 %% De-init
 pause(0.5)
